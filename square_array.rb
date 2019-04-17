e3e5d422c4b25033a12623fc73c14597a5b53816
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 00:05:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   counter = 0
   array.each do |element|
-      array[counter]element.to_i ** 2
+      array[counter] = element.to_i ** 2
       counter += 1
   end
 end
